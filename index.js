c43c8ee56d888669f99d9e54b68390b770f37df3
Nilay <rawalnilay1@gmail.com>
2023-11-13 23:46:53 +0530

    bugs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const connectMongo= require('./db')
-
+var cors = require('cors')
 const app = express();
 const port = 5000
+
+
+
+app.use(cors())
   
 // 
 app.use(express.json())
@@ -12,6 +16,6 @@ app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
-  console.log(`Example  app listening on port http://localhost:${port}`)
-})
+  console.log(`MagicSync listening on port http://localhost:${port}`)
+}) 
 connectMongo();
